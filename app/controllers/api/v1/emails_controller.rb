ef403838fa86817de44edc3f2e7394981d62de5a
Alexander Cheung <cheung.ying.lon@gmail.com>
2017-10-01 20:43:16 -0500

    standardize json response

--- app/controllers/api/v1/emails_controller.rb
+++ app/controllers/api/v1/emails_controller.rb
@@ -7,19 +7,26 @@ module Api
 
 			def create
 				if template = Template.find_by_name(params[:template][:name])
-					email = Email.new(
-						template_id: template.id,
-						to: params[:to],
-						from: params[:from],
-						subs: params[:subs],
-					)
+					email = Email.new(email_params)
+					email.template_id = template.id
 					if email.save
-						render status: :created, json: email
+						render status: 201, json: {
+							status: 201,
+							email: email,
+							template: template,
+						}
 					else
-						render status: :bad_request, json: { errors: email.errors.full_messages }
+						render status: 400, json: { 
+							status: 400,
+							errors: email.errors.full_messages,
+							template: template,
+						}
 					end
 				else
-					render status: :bad_request, json: { errors: "template does not exist" }
+					render status: 400, json: { 
+						status: 400,
+						errors: ["template does not exist"],
+					}
 				end
 			end
 			
@@ -29,6 +36,10 @@ module Api
 					authenticate_or_request_with_http_token do |token, options|
 						ApiKey.exists?(access_token: token)
 					end
+				end
+
+				def email_params
+					params.require(:email).permit(:to, :from, :template_id, :subs)
 				end
 		end
 	end
--- spec/controllers/api/v1/emails_controller_spec.rb
+++ spec/controllers/api/v1/emails_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Api::V1::EmailsController, type: :controller do
 	end
 
 	let(:email) { FactoryGirl.create(:email) }
-	let(:template) { FactoryGirl.create(:template) }
+	let(:template) { FactoryGirl.create(:template, name: "RESERVATION_CONFIRMED") }
 
 	describe "POST create" do
 		context "with valid attributes" do
@@ -15,14 +15,23 @@ RSpec.describe Api::V1::EmailsController, type: :controller do
 					template: {
 						name: template.name,
 					},
-					to: email.to,
-					from: email.from,
+					email: {
+						to: email.to,
+						from: email.from,
+					},
 				}
 				expect {
 					post :create, params: params
 				}.to change(Email, :count).by(1)
-				expect(response).to have_http_status(:created)
-				expect(JSON.parse(response.body)["status"]).to eq("pending")
+				
+				status = JSON.parse(response.body)["status"]
+				email = JSON.parse(response.body)["email"]
+				template = JSON.parse(response.body)["template"]
+
+				expect(response).to have_http_status(201)
+				expect(status).to eq(201)
+				expect(email["status"]).to eq("pending")
+				expect(template["name"]).to eq("RESERVATION_CONFIRMED")
 			end
 		end
 
@@ -32,14 +41,23 @@ RSpec.describe Api::V1::EmailsController, type: :controller do
 					template: {
 						name: template.name,
 					},
-					to: "invalid email",
-					from: email.from,
+					email: {
+						to: "invalid email",
+						from: email.from,
+					},
 				}
 				expect {
 					post :create, params: params
 				}.to_not change(Email, :count)
-				expect(response).to have_http_status(:bad_request)
-				expect(JSON.parse(response.body)["errors"]).to_not be_nil
+				
+				status = JSON.parse(response.body)["status"]
+				template = JSON.parse(response.body)["template"]
+				errors = JSON.parse(response.body)["errors"]
+
+				expect(response).to have_http_status(400)
+				expect(status).to eq(400)
+				expect(template["name"]).to eq("RESERVATION_CONFIRMED")
+				expect(errors).to_not be_nil
 			end
 		end
 
@@ -49,14 +67,23 @@ RSpec.describe Api::V1::EmailsController, type: :controller do
 					template: {
 						name: template.name,
 					},
-					to: email.to,
-					from: "invalid email",
+					email: {
+						to: email.to,
+						from: "invalid email",
+					},
 				}
 				expect {
 					post :create, params: params
 				}.to_not change(Email, :count)
-				expect(response).to have_http_status(:bad_request)
-				expect(JSON.parse(response.body)["errors"]).to_not be_nil
+				
+				status = JSON.parse(response.body)["status"]
+				template = JSON.parse(response.body)["template"]
+				errors = JSON.parse(response.body)["errors"]
+
+				expect(response).to have_http_status(400)
+				expect(status).to eq(400)
+				expect(template["name"]).to eq("RESERVATION_CONFIRMED")
+				expect(errors).to_not be_nil
 			end
 		end
 
@@ -66,14 +93,21 @@ RSpec.describe Api::V1::EmailsController, type: :controller do
 					template: {
 						name: "invalid template name",
 					},
-					to: email.to,
-					from: email.from,
+					email: {
+						to: email.to,
+						from: email.from,
+					},
 				}
 				expect {
 					post :create, params: params
 				}.to_not change(Email, :count)
-				expect(response).to have_http_status(:bad_request)
-				expect(JSON.parse(response.body)["errors"]).to_not be_nil
+								
+				status = JSON.parse(response.body)["status"]
+				errors = JSON.parse(response.body)["errors"]
+
+				expect(response).to have_http_status(400)
+				expect(status).to eq(400)
+				expect(errors).to_not be_nil
 			end
 		end
 	end
